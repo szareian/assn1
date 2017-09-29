ffe7bcbe7822311a77a5e5cbd8b77f9bc9d31442
szareian <szareian@sfu.ca>
2017-09-29 07:57:38 -0700

    assn_1 - 2

--- public/Assignment 1/calculate.js
+++ public/Assignment 1/calculate.js
@@ -1,9 +1,3 @@
-//
-// var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22,
-//   88.33, 9.03, 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44,
-//   70.54, 90.0, 71.11, 80.01];
-
-// parsefloat
 
 var lower_boundary = [];
 
@@ -23,72 +17,117 @@ lower_boundary = {
 };
 
 
+// function myfunction() {
+//   m = setInterval(getRowId,100);
+//
+// }
+
+
 function getRowId(id){
-  // alert(document.getElementById(id).value)
-  lolol = ""
-  try{
-    lolol = document.getElementById(id).value;
+  a = "";
+     a = parseFloat(document.getElementById(id).value);
 
-  } catch (e) {
-    // do nothing
-  }
 
-  num = parseFloat(lolol)
+  // num = parseFloat(lolol);
   // alert(num)
 
-  // return parseFloat(document.getElementById(id).value);
-  return num;
+  return a;
 
   // return parseFloat(document.getElementById(id).value);
   // console.log(x);
 }
 
+
 function set_boundaries(){
-  lower_boundary.max = getRowId("max");
+  try {
+    lower_boundary.max = getRowId("max");
+    lower_boundary.aPlus = getRowId("aPlus");
+    lower_boundary.a = getRowId("a");
+    lower_boundary.aMinus = getRowId("aMinus");
+    lower_boundary.bPlus = getRowId("bPlus");
+    lower_boundary.b = getRowId("b");
+    lower_boundary.bMinus = getRowId("bMinus");
+    lower_boundary.cPlus = getRowId("cPlus");
+    lower_boundary.c = getRowId("c");
+    lower_boundary.cMinus = getRowId("cMinus");
+    lower_boundary.d = getRowId("d");
+    lower_boundary.f = getRowId("f");
+  } catch (e) {
+    // do nothing
+  }
 }
 
-function pressedSomething(){
-  maxVal = getRowId("max")
+// console.log(lower_boundary);
+var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22,
+  88.33, 9.03, 49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44,
+  70.54, 90.0, 71.11, 80.01];
 
-  alert(maxVal)
+set_boundaries();
+var histogram = {
+  aPlus:0,
+  a:0,
+  aMinus:0,
+  bPlus:0,
+  b:0,
+  bMinus:0,
+  cPlus:0,
+  c:0,
+  cMinus:0,
+  d:0,
+  f:0
+};
 
+for (var i = 0; i < grades.length; i++) {
+  // console.log(grades[i]);
+  if (lower_boundary.max > grades[i] > lower_boundary.aPlus ) {
+    histogram.aPlus++;
+  }
+  else if (lower_boundary.aPlus > grades[i] > lower_boundary.a) {
+    histogram.a++;
+  }
+  else if (lower_boundary.a > grades[i] > lower_boundary.aMinus) {
+    histogram.aMinus++;
+  }
+  else if (lower_boundary.aMinus > grades[i] > lower_boundary.bPlus) {
+    histogram.bPlus++;
+  }
+  else if (lower_boundary.bPlus > grades[i] > lower_boundary.b) {
+    histogram.b++;
+  }
+  else if (lower_boundary.b > grades[i] > lower_boundary.bMinus ) {
+    histogram.bMinus++;
+  }
+  else if (lower_boundary.bMinus > grades[i] > lower_boundary.cPlus) {
+    histogram.cPlus++;
+  }
+  else if (lower_boundary.cPlus > grades[i] > lower_boundary.c) {
+    histogram.c++;
+  }
+  else if (lower_boundary.c > grades[i] > lower_boundary.cMinus) {
+    histogram.cMinus++;
+  }
+  else if (lower_boundary.cMinus > grades[i] > lower_boundary.d) {
+    histogram.d++;
+  }
+  else if (lower_boundary.d > grades[i] > lower_boundary.f) {
+    histogram.f++;
+  }
+  else{
+    // histogram.doodoo++;
+   console.log('out of range!');
+  }
 }
 
-alert(getRowId('max'));
 
-
+console.log(histogram);
 console.log(set_boundaries());
 
+// alert(getRowId('bPlus'));
 
 
+function draw_histogram() {
+  var smiley_face = "&#9786; ";
+    document.getElementById("F").innerHTML = smiley_face.repeat(histogram.f);
+}
 
-
-
-
-// console.log(getRowId('max'));
-
-
-// function myFunction() {
-//     x = document.getElementById("boundary").style.backgroundColor = "red";
-//     return x;
-// }
-
-// function myfunction(){
-//   // Get table object
-//   var myTable = document.getElementById('t1').tBodies[0];
-//
-//            // first loop for each row
-//   for (var r=0, n = myTable.rows.length; r < n; r++) {
-//     // this loop is getting each colomn/cells
-//     for (var c = 0, m = myTable.rows[r].cells.length; c < m; c++) {
-//       if(myTable.rows[r].cells[c].childNodes[0].value){
-//       // get letter grades
-//       var letterGrade = myTable.rows[r].cells[1].innerHTML;
-//       alert(letterGrade);
-//       // get student marks in each text boxes
-//       var marks = myTable.rows[r].cells[c].childNodes[0].value;
-//       alert(marks);
-//       }
-//     }
-//   }
-// }
+draw_histogram()

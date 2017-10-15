bfddf49c97b70b11e007f4bf4268db636634c561
szareian <szareian@sfu.ca>
2017-10-14 23:15:49 -0700

    fixing enrolls - try2

--- db/migrate/20171011052918_create_enrolls.rb
+++ db/migrate/20171011052918_create_enrolls.rb
@@ -3,7 +3,7 @@ class CreateEnrolls < ActiveRecord::Migration
     create_table :enrolls do |t|
       t.integer :student_id
       t.string :course_id
-      t.float :percentage, :limit =>1000
+      t.float :percentage
       t.string :lettergrade
 
       t.timestamps null: false
